4bc29260303b989791976e835954ce1c616ba462
Rahmad <107798045+Ilham1505@users.noreply.github.com>
2022-11-22 12:43:26 +0700

    Button + Slider

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private float speed = 2000;
+    private float speed = 5000;
     public int damage = 50;
     public Rigidbody2D rb;
 
